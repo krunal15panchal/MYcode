ff35320b1983065c96e742ea8b56d12d8564572b
Krunal <53361500+krunal15panchal@users.noreply.github.com>
2022-10-28 12:29:00 -0500

    Create 14.LongestCommonPrefix.cpp

---
+++ 14.LongestCommonPrefix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        int size= strs.size();
+        int min=1000,count=0;
+        string str;
+        for(int i=0;i<size;i++)
+        {
+            int len=strs[i].length();
+            if(len<min){
+                min=len;
+                str=strs[i];
+            }
+        }
+        
+        for(int i=0;i<min;i++)
+        {
+            for(int j=0;j<size;j++){
+                if(strs[j][i]!=str[i])
+                    return str.substr(0,count);
+            }
+            count++;
+        }
+        
+        return str.substr(0,count);
+    }
+};
